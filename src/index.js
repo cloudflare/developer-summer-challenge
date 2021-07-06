a43d633039da53b3cc4147161921374482eb2ad0
Luke Edwards <luke.edwards05@gmail.com>
2021-07-05 17:00:44 -0700

    chore: allow multi-page HTML builder

--- src/index.js
+++ src/index.js
@@ -2,34 +2,43 @@
 	function oninput(ev) {
 		var input = ev.target;
 		var val = (input.value || '').trim();
-		input.parentNode.classList.toggle('fill', val.length>0);
+		input.parentNode.classList.toggle('fill', val.length > 0);
 	}
 
 	function onload() {
 		var $ = document.querySelector.bind(document);
 		var i=0, inputs = document.querySelectorAll('label>input');
 
-		$('.signup form').onsubmit = async function (ev) {
-			ev.preventDefault();
-			var form = ev.target;
-			var fdata = new FormData(form);
-			var res = await fetch(form.action, {
-				method: form.method || 'POST',
-				body: fdata,
-			});
-			if (res.ok) {
-				// TODO: success screen
-				console.log('OK');
-				form.reset();
-			} else {
-				// TODO: toast box?
-				var errors = await res.json();
-				console.log('FAIl', errors);
-			}
-		}
-
 		for (; i < inputs.length; i++) {
 			inputs[i].oninput = oninput;
+		}
+
+		$('.signup form').onsubmit = async function (ev) {
+			ev.preventDefault();
+
+			var form = ev.target;
+			var res = await fetch(form.action, {
+				method: form.method || 'POST',
+				body: new FormData(form),
+			});
+
+			if (res.ok) {
+				form.reset();
+				// TODO: success screen
+				console.log('OK');
+				for (i=0; i < inputs.length; i++) {
+					input[i].parentNode.className = '';
+				}
+			} else {
+				try {
+					var error = await res.json();
+				} catch (e) {
+					// was not json
+				}
+
+				// TODO: toast box?
+				console.log('FAIL', error);
+			}
 		}
 	}
 
